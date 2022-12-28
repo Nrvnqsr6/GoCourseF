cefa700936ef7afa5ac12cb4ca689de83e922d18
Riniker <zverekqw12@gmail.com>
2022-12-28 15:46:39 +0400

    final

--- internal/action/unmarshal.go
+++ internal/action/unmarshal.go
@@ -3,9 +3,7 @@ package action
 import "encoding/json"
 
 func UnmarshalInput(body []byte) [][]int {
-	//resStep := make([][]int, 0)
 	resArray := make([][][]int, 0)
-	//json.Unmarshal([]byte(body), &resStep)
 	json.Unmarshal([]byte(body), &resArray)
 
 	value := make(([][]int), len(resArray))
